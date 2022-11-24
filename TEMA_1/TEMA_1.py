a9908af5e6d8314dca84f2d29f9fc163c0e6c640
Virgil993 <turcu.virgil@gmail.com>
2022-11-24 16:48:27 +0200

    tested solutions with score

--- TEMA_1/TEMA_1.py
+++ TEMA_1/TEMA_1.py
@@ -2,9 +2,9 @@ import cv2 as cv
 import numpy as np
 import os
 
-path_solutii = "D:\Materiale Pentru Facultate\Concepte Si Aplicatii in Vederea Artificiala\CAVA_Project_1\TEMA_1\solutii_totale\\"
+path_solutii = "D:\Materiale Pentru Facultate\Concepte Si Aplicatii in Vederea Artificiala\CAVA_Project_1\TEMA_1\solutii_scor\\"
 path_antrenare = "D:\Materiale Pentru Facultate\Concepte Si Aplicatii in Vederea Artificiala\CAVA_Project_1\TEMA_1\\antrenare\\"
-path_templates = "D:\Materiale Pentru Facultate\Concepte Si Aplicatii in Vederea Artificiala\CAVA_Project_1\TEMA_1\\templates_totale\\"
+path_templates = "D:\Materiale Pentru Facultate\Concepte Si Aplicatii in Vederea Artificiala\CAVA_Project_1\TEMA_1\\templates\\"
 
 
 def show_image(title, image):
@@ -199,6 +199,232 @@ dictionar_poz_litera = {}
 for i in range(15):
     dictionar_poz_litera[i+1]=chr(ord('A')+i)
 
+dictionar_punctaj_litera={}
+dictionar_punctaj_litera["A"]=1
+dictionar_punctaj_litera["B"]=9
+dictionar_punctaj_litera["C"]=1
+dictionar_punctaj_litera["D"]=2
+dictionar_punctaj_litera["E"]=1
+dictionar_punctaj_litera["F"]=8
+dictionar_punctaj_litera["G"]=9
+dictionar_punctaj_litera["H"]=10
+dictionar_punctaj_litera["I"]=1
+dictionar_punctaj_litera["J"]=10
+dictionar_punctaj_litera["L"]=1
+dictionar_punctaj_litera["M"]=4
+dictionar_punctaj_litera["N"]=1
+dictionar_punctaj_litera["O"]=1
+dictionar_punctaj_litera["P"]=2
+dictionar_punctaj_litera["R"]=1
+dictionar_punctaj_litera["S"]=1
+dictionar_punctaj_litera["T"]=1
+dictionar_punctaj_litera["U"]=1
+dictionar_punctaj_litera["V"]=8
+dictionar_punctaj_litera["X"]=10
+dictionar_punctaj_litera["Z"]=10
+dictionar_punctaj_litera["0"]=0
+
+def calculeaza_cuv_verticala(matrice_curenta,matrice_anterioara,pozitii):
+    poz_prima_litera = pozitii
+    # parcurgem pe verticala pana la capatul superior
+    index_1 = poz_prima_litera[0]
+    # print("index before while is "+matrice_curenta[index_1][poz_prima_litera[1]])
+    while(matrice_curenta[index_1][poz_prima_litera[1]]!='o' and index_1>=0):
+        index_1-=1
+    index_1+=1
+    scor_cuv_verticala = 0
+    triplare_cuvant = 0
+    dublare_cuvant=0
+    lungime_cuv = 0
+    # print("index is "+str(index_1))
+    # incepem sa cream cuvantul pe verticala
+    while(index_1<=14 and matrice_curenta[index_1][poz_prima_litera[1]]!='o'):
+        if(lungime_cuv==0 and index_1==14):
+            break
+        lungime_cuv+=1
+        poz_1 = index_1
+        poz_2 = poz_prima_litera[1]
+        litera_curenta = matrice_curenta[poz_1][poz_2]
+        if(matrice_anterioara[poz_1][poz_2]=='o'):
+            if(matrice_punctaje[poz_1][poz_2]==5):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+                triplare_cuvant+=1
+            if(matrice_punctaje[poz_1][poz_2]==4):
+                dublare_cuvant+=1
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+            if(matrice_punctaje[poz_1][poz_2]==3):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]*3
+            if(matrice_punctaje[poz_1][poz_2]==2):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]*2
+            if(matrice_punctaje[poz_1][poz_2]==1):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+        else:
+            scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+        index_1+=1
+    scor_cuv_verticala = scor_cuv_verticala*(2**dublare_cuvant)
+    scor_cuv_verticala = scor_cuv_verticala*(3**triplare_cuvant)
+    # print("scor cuvant pe verticala cand principala e orizontala = "+str(scor_cuv_verticala)+"\n")
+    if(lungime_cuv>1):
+        return scor_cuv_verticala
+    return 0
+
+def calculeaza_cuv_orizontala(matrice_curenta,matrice_anterioara,pozitii):
+        # parcurgem pe orizontala pana la capatul din stanga al cuvantului
+    poz_prima_litera = pozitii
+    index_1 = poz_prima_litera[1]
+    while(matrice_curenta[poz_prima_litera[0]][index_1]!='o' and index_1>=0):
+        index_1-=1
+    index_1+=1
+    scor_cuv_orizontala = 0
+    triplare_cuvant = 0
+    dublare_cuvant=0
+    lungime_cuv=0
+    # incepem sa cream cuvantul pe orizontala
+    while(index_1<=14 and matrice_curenta[poz_prima_litera[0]][index_1]!='o'):
+        if(lungime_cuv == 0 and index_1==14):
+            break
+        lungime_cuv+=1
+        poz_1 = poz_prima_litera[0]
+        poz_2 = index_1
+        litera_curenta = matrice_curenta[poz_1][poz_2]
+        if(matrice_anterioara[poz_1][poz_2]=='o'):
+            if(matrice_punctaje[poz_1][poz_2]==5):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+                triplare_cuvant+=1
+            if(matrice_punctaje[poz_1][poz_2]==4):
+                dublare_cuvant+=1
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+            if(matrice_punctaje[poz_1][poz_2]==3):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]*3
+            if(matrice_punctaje[poz_1][poz_2]==2):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]*2
+            if(matrice_punctaje[poz_1][poz_2]==1):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+        else:
+            scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+        index_1+=1
+    scor_cuv_orizontala = scor_cuv_orizontala*(2**dublare_cuvant)
+    scor_cuv_orizontala = scor_cuv_orizontala*(3**triplare_cuvant)
+    # print("index = "+str(matrice_curenta))
+    # print("scor cuvant pe orizontala cand verticala e principala = "+str(scor_cuv_orizontala)+"\n")
+    if(lungime_cuv>1):
+        return scor_cuv_orizontala
+    return 0
+
+def obtine_scor_runda(matrice_curenta,matrice_anterioara):
+    scor_total = 0
+    lista_poz_litere_noi=[]
+    for i in range(15):
+        for j in range(15):
+            if matrice_anterioara[i][j]=='o' and matrice_curenta[i][j]!='o':
+                lista_poz_litere_noi.append((i,j))
+
+    cuvant_pe_verticala = False
+    cuvant_pe_orizontala = False
+    poz_prima_litera = lista_poz_litere_noi[0]
+    if(len(lista_poz_litere_noi)>=7):
+        scor_total+=50
+    # parcurgem pe verticala pana la capatul superior
+    index_1 = poz_prima_litera[0]
+    while(matrice_curenta[index_1][poz_prima_litera[1]]!='o' and index_1>=0):
+        index_1-=1
+    index_1+=1
+    scor_cuv_verticala = 0
+    triplare_cuvant = 0
+    dublare_cuvant=0
+    lungime_cuv = 0
+    # incepem sa cream cuvantul pe verticala
+    while(index_1<=14 and matrice_curenta[index_1][poz_prima_litera[1]]!='o'):
+        if(lungime_cuv==0 and index_1==14):
+            break
+        lungime_cuv+=1
+        poz_1 = index_1
+        poz_2 = poz_prima_litera[1]
+        litera_curenta = matrice_curenta[poz_1][poz_2]
+        if(matrice_anterioara[poz_1][poz_2]=='o'):
+            if(poz_1 != poz_prima_litera[0]):
+                cuvant_pe_verticala = True
+            if(matrice_punctaje[poz_1][poz_2]==5):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+                triplare_cuvant+=1
+            if(matrice_punctaje[poz_1][poz_2]==4):
+                dublare_cuvant+=1
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+            if(matrice_punctaje[poz_1][poz_2]==3):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]*3
+            if(matrice_punctaje[poz_1][poz_2]==2):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]*2
+            if(matrice_punctaje[poz_1][poz_2]==1):
+                scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+        else:
+            scor_cuv_verticala=scor_cuv_verticala+dictionar_punctaj_litera[litera_curenta]
+        index_1+=1
+    scor_cuv_verticala = scor_cuv_verticala*(2**dublare_cuvant)
+    scor_cuv_verticala = scor_cuv_verticala*(3**triplare_cuvant)
+    # print("scor cuvant pe verticala = "+str(scor_cuv_verticala)+"\n")
+    if(lungime_cuv>1):
+        scor_total = scor_total+scor_cuv_verticala
+
+    # parcurgem pe orizontala pana la capatul din stanga al cuvantului
+    index_1 = poz_prima_litera[1]
+    while(matrice_curenta[poz_prima_litera[0]][index_1]!='o' and index_1>=0):
+        index_1-=1
+    index_1+=1
+    scor_cuv_orizontala = 0
+    triplare_cuvant = 0
+    dublare_cuvant=0
+    lungime_cuv=0
+    # incepem sa cream cuvantul pe orizontala
+    while(index_1<=14 and matrice_curenta[poz_prima_litera[0]][index_1]!='o'):
+        if(lungime_cuv==0 and index_1==14):
+            break
+        lungime_cuv+=1
+        poz_1 = poz_prima_litera[0]
+        poz_2 = index_1
+        litera_curenta = matrice_curenta[poz_1][poz_2]
+        if(matrice_anterioara[poz_1][poz_2]=='o'):
+            if(poz_2 != poz_prima_litera[1]):
+                cuvant_pe_orizontala = True
+            if(matrice_punctaje[poz_1][poz_2]==5):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+                triplare_cuvant+=1
+            if(matrice_punctaje[poz_1][poz_2]==4):
+                dublare_cuvant+=1
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+            if(matrice_punctaje[poz_1][poz_2]==3):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]*3
+            if(matrice_punctaje[poz_1][poz_2]==2):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]*2
+            if(matrice_punctaje[poz_1][poz_2]==1):
+                scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+        else:
+            scor_cuv_orizontala=scor_cuv_orizontala+dictionar_punctaj_litera[litera_curenta]
+        index_1+=1
+    scor_cuv_orizontala = scor_cuv_orizontala*(2**dublare_cuvant)
+    scor_cuv_orizontala = scor_cuv_orizontala*(3**triplare_cuvant)
+    # print("dublare cuvant = "+str(dublare_cuvant))
+    # print("scor cuvant pe orizontala = "+str(scor_cuv_orizontala)+"\n")
+    if(lungime_cuv>1):
+        scor_total = scor_total+scor_cuv_orizontala
+
+    if(cuvant_pe_orizontala and cuvant_pe_verticala):
+        print("error")
+        exit(1)
+    if(cuvant_pe_verticala):
+        for pozitii in lista_poz_litere_noi[1:]:
+            scor_total = scor_total+calculeaza_cuv_orizontala(matrice_curenta,matrice_anterioara,pozitii)
+    else:
+        for pozitii in lista_poz_litere_noi[1:]:
+            scor_total = scor_total+calculeaza_cuv_verticala(matrice_curenta,matrice_anterioara,pozitii)
+    return scor_total
+
+
+
+
+matrice_initiala = np.empty((15,15),dtype="str")
+for i in range(15):
+    for j in range(15):
+        matrice_initiala[i][j]='o'
 
 
 def scrie_rezultate(path_train,path_sol,path_temp):
@@ -206,10 +432,14 @@ def scrie_rezultate(path_train,path_sol,path_temp):
     lista_rezultate = []
     files=os.listdir(path_train)
     game_nr= 1
+    matrice_anterioara=matrice_initiala
     for file in files:
+        # if(file[-8:] != "3_05.jpg" and file[-8:] != "3_06.jpg"):
+        #         continue
         if file[-3:]=='jpg':
             if(file[-6:] == "01.jpg"):
                 matrice_viz = np.zeros((15,15),dtype=int)
+                matrice_anterioara=matrice_initiala
                 if(len(lista_rezultate)!=0):
                     for i in range(len(lista_rezultate)):
                         if i >=0 and i <=8:
@@ -220,8 +450,6 @@ def scrie_rezultate(path_train,path_sol,path_temp):
                             file_writer.write(element+"\n")
                         file_writer.close
                 lista_rezultate = []
-            # if(file[-6:] == "20.jpg"):
-            #     break
             lista_runda = []
             img = cv.imread(path_train+file)
             result=extrage_careu(img)
@@ -239,9 +467,12 @@ def scrie_rezultate(path_train,path_sol,path_temp):
                                 lista_runda.append(str(i+1)+dictionar_poz_litera[j+1]+" "+"?")
                             else:
                                 lista_runda.append(str(i+1)+dictionar_poz_litera[j+1]+" "+matrice[i][j])
-            # print(str(lista_runda)+"\n")
+            scor_runda = obtine_scor_runda(matrice,matrice_anterioara)
+            # print("scor final = "+str(scor_runda))
+            lista_runda.append(str(scor_runda))
             lista_rezultate.append(lista_runda)
             game_nr = int(file[-8])
+            matrice_anterioara=matrice
     for i in range(len(lista_rezultate)):
         if i >=0 and i <=8:
             file_writer = open(path_sol+str(game_nr)+"_0"+str(i+1)+".txt","w")
